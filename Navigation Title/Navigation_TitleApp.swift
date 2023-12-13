3f5eea8a116cfa595633058425a2a31fbdca63e1
joy jain <joyshah204@gmial.com>
2023-12-13 17:39:14 +0530

    Initial Commit

---
+++ Navigation Title/Navigation_TitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_TitleApp.swift
+//  Navigation Title
+//
+//  Created by JOY JAIN on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_TitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
